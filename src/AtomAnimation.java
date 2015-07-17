c3a620f6ff4ba5a10b6d39b7fd32f884b14fb618
collindutter <collindutter@gmail.com>
2015-07-16 19:17:15 -0700

    Setup basis for inheritence tree of atom models

--- src/AtomAnimation.java
+++ src/AtomAnimation.java
@@ -11,6 +11,7 @@ public class AtomAnimation extends PApplet
     private float cameraX, cameraY;
     private float scaleFactor;
     private DaltonModel daltonModel;
+    private ThomsonModel thomsonModel;
 
     public AtomAnimation()
     {
@@ -18,6 +19,7 @@ public class AtomAnimation extends PApplet
         cameraY = 0;
         scaleFactor = 1;
         daltonModel = new DaltonModel();
+        thomsonModel = new ThomsonModel();
     }
 
     public void setup()
@@ -27,13 +29,20 @@ public class AtomAnimation extends PApplet
 
     public void draw()
     {
+        //set background and basic stroke colors
         background(255);
         stroke(0);
+
         pushMatrix();
+        //scale based on zoom
         translate(width / 2, height / 2);
         scale(scaleFactor);
         translate(-width / 2, -height / 2);
-        daltonModel.draw(width / 2, height / 2);
+
+
+        //draw the dalton model
+        thomsonModel.draw(width / 2, height / 2);
+
         popMatrix();
     }
 
@@ -44,15 +53,26 @@ public class AtomAnimation extends PApplet
     }
 
 
-    private class DaltonModel
+    private class AtomModel
     {
         private String name;
         private int radius;
 
+        public AtomModel(String name, int radius)
+        {
+            this.name = name;
+            this.radius = radius;
+        }
+
+        public String getName(){return name;}
+        public int getRadius(){return radius;}
+    }
+
+    private class DaltonModel extends AtomModel
+    {
         public DaltonModel()
         {
-            name = "Dalton Model";
-            radius = 25;
+            super("Dalton Model", 25);
         }
 
         /**
@@ -66,10 +86,38 @@ public class AtomAnimation extends PApplet
             pushMatrix();
             translate(centerX, centerY);
             fill(205, 103, 204);
-            ellipse(0, 0, radius * 2, radius * 2);
+            ellipse(0, 0, getRadius() * 2, getRadius() * 2);
+            fill(0);
             textSize(15);
             textAlign(CENTER);
-            text(name, 0, radius + 15);
+            text(getName(), 0, getRadius() + 15);
+            popMatrix();
+        }
+    }
+
+    private class ThomsonModel extends AtomModel
+    {
+        public ThomsonModel()
+        {
+            super("Thomson Model", 25);
+        }
+
+        /**
+         * draws the Thomson model of the atom
+         *
+         * @param centerX x coordinate to center the atom upon
+         * @param centerY y coordinate to center the atom upon
+         */
+        public void draw(float centerX, float centerY)
+        {
+            pushMatrix();
+            translate(centerX, centerY);
+            fill(205, 103, 204);
+            ellipse(0, 0, getRadius() * 2, getRadius() * 2);
+            fill(0);
+            textSize(15);
+            textAlign(CENTER);
+            text(getName(), 0, getRadius() + 15);
             popMatrix();
         }
     }
